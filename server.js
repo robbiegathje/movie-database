1301682f360355602290b217a4eb09e039a77b53
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-08-28 22:26:48 -0400

    fix: change port in production

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
-app.listen(3001, () => {
-	console.log('listening on port 3001');
+const PORT = +process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+	console.log(`listening on port ${PORT}`);
 });
