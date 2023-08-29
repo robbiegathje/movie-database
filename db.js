81dabcb53c26cc6d160f6e1565f70e06d6243610
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-08-28 22:19:15 -0400

    fix: fix database URL in production

--- db.js
+++ db.js
@@ -8,6 +8,10 @@ if (process.env.NODE_ENV === 'test') {
 	DB_URI = process.env.DATABASE_URL || 'postgresql:///movies';
 }
 
+if (DB_URI.startsWith('postgres://')) {
+	DB_URI = DB_URI.replace('postgres://', 'postgresql://');
+}
+
 let db = new Client({
 	connectionString: DB_URI,
 });
