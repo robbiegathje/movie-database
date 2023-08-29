ec9d6b67d8f4e113f5276457439bf4e3b74fa9af
James (Robbie) Gathje <robbiegathje@outlook.com>
2023-08-28 22:01:27 -0400

    chore: add production database URL

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ let DB_URI = '';
 if (process.env.NODE_ENV === 'test') {
 	DB_URI = 'postgresql:///movies_test';
 } else {
-	DB_URI = 'postgresql:///movies';
+	DB_URI = process.env.DATABASE_URL || 'postgresql:///movies';
 }
 
 let db = new Client({
